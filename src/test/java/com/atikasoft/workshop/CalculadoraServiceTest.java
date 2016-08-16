decb012f25800e7c206ad39a944fbdb8e1e59087
Henry Molina <henry.molina@atikasoft.com.ec>
2016-08-15 19:21:00 -0500

    ok

---
+++ src/test/java/com/atikasoft/workshop/CalculadoraServiceTest.java
@@ -0,0 +1,18 @@
+package com.atikasoft.workshop;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.atikasoft.workshop.services.CalculadoraService;
+
+public class CalculadoraServiceTest {
+
+	@Test
+	public void testSuma() {
+		CalculadoraService d = new CalculadoraService();
+		assertEquals("La suma de 1 + 2 = 3", d.suma(1, 2));
+
+	}
+
+}
